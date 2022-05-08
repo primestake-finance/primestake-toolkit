17a58989188e54a4355b1f7ff0e2346d8ac56f20
Literally Collective <bluedragon.cstar@outlook.com>
2022-05-08 10:50:55 -0300

    Create Footer.tsx

---
+++ packages/pancake-uikit/src/components/Footer/Footer.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { baseColors, darkColors, lightColors } from "../../theme/colors";
+import { Flex, Box } from "../Box";
+import { Link } from "../Link";
+import {
+  StyledFooter,
+  StyledIconMobileContainer,
+  StyledList,
+  StyledListItem,
+  StyledText,
+  StyledSocialLinks,
+  StyledToolsContainer,
+} from "./styles";
+import { FooterProps } from "./types";
+import { ThemeSwitcher } from "../ThemeSwitcher";
+import LangSelector from "../LangSelector/LangSelector";
+import CakePrice from "../CakePrice/CakePrice";
+import { LogoWithTextIcon, ArrowForwardIcon } from "../Svg";
+import { Button } from "../Button";
+import { Colors } from "../..";
+
+const MenuItem: React.FC<FooterProps> = ({
+  items,
+  isDark,
+  toggleTheme,
+  currentLang,
+  langs,
+  setLang,
+  cakePriceUsd,
+  buyCakeLabel,
+  ...props
+}) => {
+  return (
+    <StyledFooter p="25px 16px" {...props} justifyContent="center">
+      <Flex flexDirection="column" width={["100%", null, "1200px;"]}>
+        <StyledToolsContainer
+          order={[1, null, 3]}
+          flexDirection={["column", null, "row"]}
+          justifyContent="space-between"
+        >
+          <StyledSocialLinks order={[2]} alignItems='center' />
+          <Flex order={[1, null, 2]} mb={["24px", null, "0"]} justifyContent="space-between" alignItems="center">
+            <Box mr="20px">
+              <CakePrice cakePriceUsd={cakePriceUsd} color={darkColors.textSubtle as keyof Colors} />
+            </Box>
+            <Button
+              as="a"
+              href="https://spookyswap.finance/swap"
+              target="_blank"
+              scale="sm"
+              endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
+            >
+              {buyCakeLabel}
+            </Button>
+          </Flex>
+        </StyledToolsContainer>
+      </Flex>
+    </StyledFooter>
+  );
+};
+
+export default MenuItem;
